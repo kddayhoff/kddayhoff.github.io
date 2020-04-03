3087b34f2c0612825ba0177a6581c8c1e95faa2c
Karla Dayhoff <kddayhoff@gmail.com>
2020-04-02 23:35:54 -0500

    added photo to first collapsible, made links on navbar jump to correct sections on page, made project cards animated

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,18 @@
 console.log("sit quietly and watch the grass grow");
 
+
+
+
 $(document).ready(function(){
     $('.collapsible').collapsible({
         inDuration: 600,
         outDuration: 600,
         accordion: true
     });
-  });
+
+    $('.sidenav').sidenav();
+});
+
+
 
  
